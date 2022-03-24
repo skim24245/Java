c08e8c52b7496305f08a7ce75d1aba63207ddda3
sunyoung kim <sna_2@naver.com>
2022-03-24 10:27:07 +0900

    add files

---
+++ Lecture/src/kr/ac/kopo/day11/CalendarMain.java
@@ -0,0 +1,13 @@
+package kr.ac.kopo.day11;
+
+public class CalendarMain {
+	
+		public static void main(String[] args) {
+		
+			CalendarView view = new CalendarView();
+			view.process();
+			
+		}
+	}
+
+
